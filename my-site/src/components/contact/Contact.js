64902b1706478ed57b5accd533f254d568d9401b
Cristian <cristian123105@gmail.com>
2020-02-02 20:04:38 -0300

    ref project

--- my-site/src/components/contact/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-dom';
-
-const Contact = () =>{
-    return(
-        <h1>Contact</h1>
-    )
-}
-
-export default Contact;
--- my-site/src/components/skills/Skills.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-dom';
-
-const Skills = () =>{
-    return(
-        <h1>Skills</h1>
-    )
-}
-
-export default Skills;
