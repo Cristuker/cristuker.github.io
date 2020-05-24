bc45ffd6c74671d3ac3493a061a50ffcb021ec06
Cristian Silva <cristian123105@gmail.com>
2020-05-24 10:47:33 -0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 (function redirec(){
-    window.location.href = "https://cristuker.github.io/index.html";
-})()
+    if(!window.location.href != "https://cristuker.github.io/index.html") 
+        window.location.href != "https://cristuker.github.io/index.html";
+})();
